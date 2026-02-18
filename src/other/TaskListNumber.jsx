e9cddf748b5f8ca429fdcf6c68de03ec825a8980
Biswajit Rout <routhappy9876@gmail.com>
2026-02-18 23:50:28 +0530

    update on ems

---
+++ src/other/TaskListNumber.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TaskListNumber = () => {
+  return (
+    <div className='flex justify-between gap-5 screen'>
+      <div className='h-40 w-1/2 bg-red-400'></div>
+      
+    </div>
+  )
+}
+
+export default TaskListNumber
+
