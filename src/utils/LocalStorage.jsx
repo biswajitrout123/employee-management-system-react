15ac5d88ddc1c50428d5b07971dde37dfca846eb
Biswajit Rout <routhappy9876@gmail.com>
2026-02-20 20:26:56 +0530

    update in ems

--- src/utils/LocalStorage.jsx
+++ src/utils/LocalStorage.jsx
@@ -205,8 +205,15 @@ const admin = [
 ];
 
 export const setLocalStorage = () => {
-  localStorage.setItem('employees', employees)
+  localStorage.setItem('employees', JSON.stringify(employees))
+  localStorage.setItem('admin', JSON.stringify(admin))
+
 }
 export const getLocalStorage = () => {
-
+  const employees = JSON.parse(localStorage.getItem('employees'))
+  const admin = JSON.parse(localStorage.getItem('admin'))
+  console.log(employees);
+  console.log(admin);
+  
+  
 }
